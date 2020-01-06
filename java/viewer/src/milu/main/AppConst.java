3462e3ae0860ae14e543d3290186145e16c1f1ac
milu(Mitsuharu Kiriu) <milu.kiriu2010@gmail.com>
2020-01-07 08:43:01 +0900

    update version dialog

--- java/viewer/src/milu/main/AppConst.java
+++ java/viewer/src/milu/main/AppConst.java
@@ -6,7 +6,7 @@ import java.io.File;
 public enum AppConst 
 {
 	APP_NAME("MiluDBViewer"),
-	VER("0.3.6"),
+	VER("0.3.7"),
 	UPDATE_DATE("2019/09/18"),
 	USER_DIR(System.getProperty("user.home")+File.separator+".MiluDBViewer"+File.separator),
 	APP_CONF(USER_DIR.val()+"app_conf"+File.separator+"app_conf.json"),
