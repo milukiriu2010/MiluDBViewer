d2c3454d1e4fd72986d0e5b79490862cdbac7543
milukiri2010 <milu.kiriu2010@tmail.com>
2024-03-22 11:30:34 +0900

    oracle database 23c free

--- sql/oracle/a01.ora.create_user.sql
+++ sql/oracle/a01.ora.create_user.sql
@@ -1,8 +1,18 @@
-create tablespace miludbdata datafile 'c:\myora\miludb\miludb_data01.dbf' size 100M;
+-- create tablespace miludbdata datafile 'c:\myora\miludb\miludb_data01.dbf' size 100M;
 
-create temporary tablespace miludbtemp tempfile 'c:\myora/miludb/miludb_temp01.dbf' size 100M autoextend off;
+-- create temporary tablespace miludbtemp tempfile 'c:\myora/miludb/miludb_temp01.dbf' size 100M autoextend off;
 
-create user milu identified by milu default tablespace miludbdata temporary tablespace miludbtemp quota unlimited on users account unlock;
+-- create user milu identified by milu default tablespace miludbdata temporary tablespace miludbtemp quota unlimited on users account unlock;
+
+-- alter pluggable database miludbp open;
+-- alter session set container = miludbp;
+
+alter pluggable database freepdb1 open;
+alter session set container = freepdb1;
+
+
+create user milu identified by milu default tablespace users temporary tablespace temp quota unlimited on users account unlock;
+
 
 grant connect to milu;
 grant create table to milu;
