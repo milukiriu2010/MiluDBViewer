8e60c1005d1c6817ceda48c288c2a97fcacc0a85
milukiri2010 <milu.kiriu2010@tmail.com>
2024-03-26 14:29:54 +0900

    mysql modify create user

--- sql/mysql/b01.mysql.grant.sql
+++ sql/mysql/b01.mysql.grant.sql
@@ -4,32 +4,32 @@
 -- # 
 -- # 
 
-create user 'milu'@'localhost' identified by 'milu';
+create user 'milu'@'%' identified by 'milu';
 
-grant select on *.* to 'milu'@'localhost';
-grant insert on *.* to 'milu'@'localhost';
-grant update on *.* to 'milu'@'localhost';
-grant delete on *.* to 'milu'@'localhost';
-grant execute on *.* to 'milu'@'localhost';
-grant create on *.* to 'milu'@'localhost';
-grant create routine on *.* to 'milu'@'localhost';
-grant create view on *.* to 'milu'@'localhost';
-grant show view on *.* to 'milu'@'localhost';
-grant trigger on *.* to 'milu'@'localhost';
-grant drop on *.* to 'milu'@'localhost';
-grant alter on *.* to 'milu'@'localhost';
-grant references on *.* to 'milu'@'localhost';
-grant index on miludb.* to 'milu'@'localhost';
+grant select on *.* to 'milu'@'%';
+grant insert on *.* to 'milu'@'%';
+grant update on *.* to 'milu'@'%';
+grant delete on *.* to 'milu'@'%';
+grant execute on *.* to 'milu'@'%';
+grant create on *.* to 'milu'@'%';
+grant create routine on *.* to 'milu'@'%';
+grant create view on *.* to 'milu'@'%';
+grant show view on *.* to 'milu'@'%';
+grant trigger on *.* to 'milu'@'%';
+grant drop on *.* to 'milu'@'%';
+grant alter on *.* to 'milu'@'%';
+grant references on *.* to 'milu'@'%';
+grant index on miludb.* to 'milu'@'%';
 
-grant execute on *.* to 'milu'@'localhost';
+grant execute on *.* to 'milu'@'%';
 -- # grant execute on *.* to 'milu'@'%';
 
 flush privileges;
 
 -- # ---------------------------------------------------
 
-create user 'user01'@'localhost' identified by 'user01';
+create user 'user01'@'%' identified by 'user01';
 
-grant select on sakila.* to 'user01'@'localhost';
+grant select on sakila.* to 'user01'@'%';
 
 
