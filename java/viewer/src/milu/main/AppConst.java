d858dbc48d3651afece5385d6c6f2c4047d8bbeb
milukiriu2010 <milu.kiriu2010@gmail.com>
2026-09-24 10:07:57 +0900

    jdk27

--- java/viewer/src/milu/main/AppConst.java
+++ java/viewer/src/milu/main/AppConst.java
@@ -6,8 +6,8 @@ import java.io.File;
 public enum AppConst 
 {
 	APP_NAME("MiluDBViewer"),
-	VER("0.4.0_jdk26"),
-	UPDATE_DATE("2026/04/04"),
+	VER("0.4.0_jdk27"),
+	UPDATE_DATE("2026/09/24"),
 	USER_DIR(System.getProperty("user.home")+File.separator+".MiluDBViewer"+File.separator),
 	APP_CONF(USER_DIR.val()+"app_conf"+File.separator+"app_conf.json"),
 	DRIVER_DIR(USER_DIR.val()+"driver"+File.separator),
